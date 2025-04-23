cf57e9d00884204b82bf1ae83d3208cb020dc286
23DP1Lvidr <A230089LV@rvt.lv>
2025-04-23 09:50:07 +0300

    patientmanager improvements

--- PatientManager.java
+++ PatientManager.java
@@ -11,19 +11,8 @@ public class PatientManager {
 
         PatientCount = 0;
  
-        ArrayList<Patient> patientList = new ArrayList<>();
-        BufferedReader reader = Helper.getReader("Patients.csv");
-        reader.readLine();
-        String line;
- 
- 
-        while ((line = reader.readLine()) != null) {
- 
-            String[] parts = line.split(",");
-            Patient patient = new Patient(parts[0],(parts[1]),(parts[2]),(parts[3]));
- 
-            patientList.add(patient);
-        }
+        PatientManager patientManager = new PatientManager();
+        ArrayList<Patient> patientList = patientManager.getPatientArrayList();
 
         for (Patient patient : patientList){
             System.out.printf("|%-19s| %-19s| %-19s| %-34s| \n",patient.GetName(),patient.GetSurname(),patient.GetBirthdate(),patient.GetEmail());
@@ -42,6 +31,18 @@ public class PatientManager {
 
     public void findPatient(String keyword) throws Exception{
 
+        PatientManager patientManager = new PatientManager();
+        ArrayList<Patient> patientList = patientManager.getPatientArrayList();
+
+        for (Patient patient : patientList) {
+            if (patient.matches(keyword)) {
+                System.out.printf("|%-19s| %-19s| %-19s| %-34s| \n",patient.GetName(),patient.GetSurname(),patient.GetBirthdate(),patient.GetEmail());
+            }
+        }
+    }
+
+    public ArrayList getPatientArrayList() throws Exception{
+        
         ArrayList<Patient> patientList = new ArrayList<>();
         BufferedReader reader = Helper.getReader("Patients.csv");
         reader.readLine();
@@ -56,10 +57,6 @@ public class PatientManager {
             patientList.add(patient);
         }
 
-        for (Patient patient : patientList) {
-            if (patient.matches(keyword)) {
-                System.out.printf("|%-19s| %-19s| %-19s| %-34s| \n",patient.GetName(),patient.GetSurname(),patient.GetBirthdate(),patient.GetEmail());
-            }
-        }
+        return patientList;
     }
 }
